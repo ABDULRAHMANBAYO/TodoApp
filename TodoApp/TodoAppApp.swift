d828e28e8dcc84482b85d4bc331af4fa939582c7
abdulrahmanbayo <ramhziworld@gmail.com>
2022-11-21 22:27:59 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Kole-ibrahim Abdul rahman on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
